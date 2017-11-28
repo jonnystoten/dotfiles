86c8343ae1fcc8c6c2ee671adfb895e48c519776
Jonny Stoten <jonny@rocketmakers.com>
2017-11-28 08:19:45 +0000

    fish: set up ssh-agent on first shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,6 +17,14 @@ if reboot_check
   echo "The kernel is out of date, it's time to reboot!"
 end
 
+if not pgrep -u "$USER" ssh-agent > /dev/null
+  ssh-agent -c > ~/.ssh-agent-thing
+  chmod +x ~/.ssh-agent-thing
+end
+if [ "$SSH_AGENT_PID" = "" ]
+  eval (cat ~/.ssh-agent-thing)
+end
+
 if status is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
     exec startx

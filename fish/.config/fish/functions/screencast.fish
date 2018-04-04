57ef0d7af75800e2843f7ae7069762971cc6919c
Jonny Stoten <jonny@jonnystoten.com>
2018-04-04 20:55:10 +0100

    Add scripts and i3 config for taking screenshots/casts

---
+++ fish/.config/fish/functions/screencast.fish
@@ -0,0 +1,16 @@
+function screencast
+  set tmpfile (mktemp -t screencast-XXXXXXXX).mkv
+  set output $HOME/screencasts/(date +%F-%H-%M-%S)
+
+  trap "rm -f $tmpfile" EXIT
+
+  slop -f "%x %y %w %h" | read x y w h
+  ffmpeg -f x11grab -s {$w}x{$h} -i :0.0+$x,$y $tmpfile
+
+  ffmpeg -y -i $tmpfile -vf fps=10,palettegen /tmp/palette.png
+  ffmpeg -i $tmpfile -i /tmp/palette.png -filter_complex "paletteuse" $output.gif
+
+  mv $tmpfile $output.mkv
+
+  mpv $output.gif
+end
---
+++ fish/.config/fish/functions/screenshot.fish
@@ -0,0 +1,14 @@
+function screenshot
+  set tmpfile (mktemp -t screenshots-XXXXXXXX).png
+  set output $HOME/screenshots/(date +%F-%H-%M-%S)
+
+  trap "rm -f $tmpfile" EXIT
+
+  slop -f "%x %y %w %h" | read x y w h
+  ffmpeg -f x11grab -s {$w}x{$h} -i :0.0+$x,$y -vframes 1 $tmpfile
+
+  mv $tmpfile $output.png
+
+  feh $output.png
+end
+
